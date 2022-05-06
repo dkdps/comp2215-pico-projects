5cc94377dd82339a96eeda7727ef043624f58b69
dkdps <champion.pillip@gmail.com>
2022-05-06 22:13:14 +0100

    Added pico sd

---
+++ picosd/SDCard.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "ff.h"
+#include "f_util.h"
+#include "hw_config.h"
+#include "rtc.h"
+
+int main() {
+    stdio_init_all();
+    time_init();
+
+    puts("Hello, world!");
+
+    // See FatFs - Generic FAT Filesystem Module, "Application Interface",
+    // http://elm-chan.org/fsw/ff/00index_e.html
+    sd_card_t *pSD = sd_get_by_num(0);
+
+    // Register the work area of the volume.
+    FRESULT fr = f_mount(&pSD->fatfs, pSD->pcName, 1);
+    if (FR_OK != fr) panic("f_mount error: %s (%d)\n", FRESULT_str(fr), fr);
+
+    // Create a file.
+    FIL fil;
+    const char* const filename = "filename.txt";
+    fr = f_open(&fil, filename, FA_OPEN_APPEND | FA_WRITE);
+    if (FR_OK != fr && FR_EXIST != fr)
+        panic("f_open(%s) error: %s (%d)\n", filename, FRESULT_str(fr), fr);
+    
+    // Write to the file.
+    if (f_printf(&fil, "Hello, world!\n") < 0) {
+        printf("f_printf failed\n");
+    }
+
+    // Close the file.
+    fr = f_close(&fil);
+    if (FR_OK != fr) {
+        printf("f_close error: %s (%d)\n", FRESULT_str(fr), fr);
+    }
+
+    // Deregister the workspace.
+    f_unmount(pSD->pcName);
+
+    puts("Goodbye, world!");
+    for(;;);
+    
+    
+}
+
